6c2849c60cab1bb8ed27ca441c6dcd4bfd201926
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-02-18 20:21:39 +0600

    seat selection running

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,41 @@
 // select the seats function
-function seatSelect() {
+let SelectYourSeatClass = document.getElementsByClassName('SelectYourSeat');
+for (let SelectYourSeat of SelectYourSeatClass) {
+    SelectYourSeat.addEventListener('click', function (event) {
+        let getTheId = event.target.innerText;
+        document.getElementById(getTheId).classList.add('bg-[#1cd100]');
 
+        priceListfunction(getTheId);
+
+        counterChange(getTheId);
+
+    });
 }
-// let getf = document.getElementsByClassName('SelectYourSeat').addEventListener('')
-// console.log(getf);
+
+function priceListfunction(getTheId) {
+    let priceList = document.getElementById('priceList');
+    let divP = document.createElement('div');
+    divP.classList.add('flex', 'flex-row', 'justify-between');
+    let getP = document.createElement('p');
+    getP.innerText = getTheId;
+    divP.appendChild(getP);
+    let classP = document.createElement('p');
+    classP.innerText = 'AC_Business';
+    divP.appendChild(classP);
+    let priceP = document.createElement('p');
+    priceP.innerText = '550';
+    divP.appendChild(priceP);
+    priceList.appendChild(divP);
+}
+
+let count = 0;
+
+function counterChange(getTheId){ 
+    let priceList = document.getElementById('selectedSeat');
+    count = count + 1;
+    console.log(count);
+    priceList.innerText = count;
+}
 
 // next button fuction
 document.getElementById('next').addEventListener('click', function () {
